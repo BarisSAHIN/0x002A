dc22c747c8c64441ec32ae8427134c628d551104
Ongun Alp Baba <revengeangle324@gmail.com>
2018-04-11 20:47:39 +0300

    Player Story yapılana kadar hazır değil

--- 0x002a/src/sample/Player.java
+++ 0x002a/src/sample/Player.java
@@ -1,14 +1,38 @@
 package sample;
 
+import java.util.Scanner;
+
 public class Player extends User {
 
+    private Story theStory;
+
+    public Player(){
+        theStory=new Story(pickGame());
+    }
+
+    public void play(){
+        while(theStory.isEnd())
+            giveAnswer();
+        finish();
+    }
+
+    /**
+     * Gets game file from user and returns informations to Application by filling a Story object.
+     */
+    public String pickGame(){
+        System.out.println("Enter game file name you want to play: ");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
     public void giveAnswer(){
-
+        System.out.println("Your Answer: ");
+        Scanner scanner = new Scanner(System.in);
+        if(!(theStory.ifLegalGetNextQuestion(scanner.nextLine())))
+            giveAnswer();
     }
-    public void getEffect(){
 
-    }
     public void finish(){
-
++
     }
 }
