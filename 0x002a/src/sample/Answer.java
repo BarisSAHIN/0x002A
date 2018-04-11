79a9203b1f5f5faa338b0d2fc10d43c038384ea0
Vefik Fırat Akman <vfakman@gmail.com>
2018-04-11 17:24:07 +0300

    Update Answer.java

--- 0x002a/src/sample/Answer.java
+++ 0x002a/src/sample/Answer.java
@@ -1,20 +1,46 @@
 package sample;
 
+import java.util.HashMap;
+import java.util.Scanner;
+import javafx.util.Pair;
+
 public class Answer extends Node {
-    public Question nextLeft;
-    public Question nextRight;
-
-    public void GetSideEffects(){
-
+    public Question next;
+    String answerText;
+    HashMap<String, Pair<Character, Integer>> statsToBeChanged;
+    Answer(){
+        next = null;
+        writeAnswer();
     }
-    public void GetNextQuestion(){
 
+    private void writeAnswer(){
+        Scanner sc = new Scanner(System.in);
+        answerText = sc.next();
     }
-    public void GetAvailableQuestions(){
 
+    public void showAnswer(){
+        System.out.println(answerText);
     }
-    public void AddSideEffect(){
 
+    public Question GetNextQuestion(){
+        return next;
+    }
+
+    public void setNextQuestion(Question nextQuestion){
+        next = nextQuestion;
+    }
+
+    public boolean hasNextQuestion(){
+        if( next != null){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public HashMap<String, Pair<Character, Integer>> changeStats(){
+        return statsToBeChanged;
     }
 
 }
