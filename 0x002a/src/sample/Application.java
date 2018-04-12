72ce0f8aea9460b8e1395c41939bd45e87f4ad57
BarisSAHIN <0barissahin0@gmail.com>
2018-04-12 22:21:29 +0300

    Application update

--- 0x002a/src/sample/Application.java
+++ 0x002a/src/sample/Application.java
@@ -1,20 +1,44 @@
 package sample;
 
+import java.util.Scanner;
+
 public class Application {
-    public User currUser;
-    public Node startNode;
 
-    public void getCharacterStats(){
-    String abcd;
-    }
-    public void movetoNext(){
+    int user=0;
+    Story story;
 
-    }
-    public void updateCharacter(){
+    public void mainMenu(){
 
-    }
-    public void getAquestion(){
+        Scanner chooseScanner = new Scanner(System.in);
 
+        System.out.println("-------------------------------------------");
+        System.out.println("-------------------------------------------");
+        System.out.println("-------------- W E L C O M E --------------");
+        System.out.println("------------------ T H E ------------------");
+        System.out.println("--------------- 0 x 0 0 2 A ---------------");
+        System.out.println("-------------------------------------------");
+        System.out.println("-------------------------------------------\n\n");
+
+
+        System.out.println(" 1) Player\n 2) Maker");
+        user = chooseScanner.nextInt();
+
+        if(user==1){
+
+            Player gamePlayer= new Player();
+            gamePlayer.play();
+
+        }
+        else if(user==2){
+            Maker gameMaker = new Maker();
+            gameMaker.createStory();
+
+        }
+        // take user from screen(some javafx  shit);
     }
-    
+
+
+
+
+
 }
