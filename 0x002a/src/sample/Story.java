68ba651a5a1bb146c5159d6cc318c9fc272a862b
BarisSAHIN <0barissahin0@gmail.com>
2018-04-18 19:25:44 +0300

    Application update

---
+++ 0x002a/src/sample/Story.java
@@ -0,0 +1,33 @@
+package sample;
+
+public class Story {
+
+    private Question firtQuestion=null;
+    private CStats GameChar=null;
+    private Question currQuestion = null;
+    public Story(String fileName){
+
+        initializeStory(fileName);
+
+    }
+
+    public void initializeStory(String fileName){
+        //Verilen file'dan initialize et
+        return;
+    }
+    public void saveStory(){
+
+    }
+    public boolean isFinish(){
+        return currQuestion.isEnd();
+    }
+    public void showQuestion(){
+
+        currQuestion.showQuestion();
+
+    }
+    public boolean ifLegalGetNextQuestion(String Answer){
+        return false;
+    }
+
+}
