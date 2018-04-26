7c40aebcf74b3c7471e50712f9b0ee51ab440b54
Gunseli Ozkan <gns.ozkan@gmail.com>
2018-04-26 22:39:21 +0300

    SetText changed (no more waiting for input)

--- 0x002a/src/sample/Question.java
+++ 0x002a/src/sample/Question.java
@@ -1,9 +1,9 @@
 package sample;
 
 import javafx.util.Pair;
+
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Scanner;
 
 /**
  *      This class is needed for game's/application's Questions.
@@ -40,7 +40,7 @@ public class Question extends Node implements Comparable {
     public Question() throws IDNotAllowed {
         Answers = new ArrayList<Answer>();
         setAnswers(null);
-        setText();
+        setText(" ");
         setId(0);
         preRequisite = new HashMap<>();
     }
@@ -52,7 +52,7 @@ public class Question extends Node implements Comparable {
     public Question(int id) throws IDNotAllowed {
         Answers = new ArrayList<Answer>();
         setAnswers(null);
-        setText();
+        setText(" ");
         setId(id);
         preRequisite = new HashMap<>();
     }
@@ -65,7 +65,7 @@ public class Question extends Node implements Comparable {
     public Question(ArrayList<Answer> a){
         Answers = new ArrayList<Answer>();
         setAnswers(a);
-        setText();
+        setText(" ");
         preRequisite = new HashMap<>();
     }
 
@@ -146,9 +146,8 @@ public class Question extends Node implements Comparable {
     /**
      *  Setter for Question Text.
      */
-    private void setText(){
-        Scanner sc = new Scanner(System.in);
-        QuestionText = sc.next();
+    private void setText(String s){
+        QuestionText = s;
     }
 
     /**
